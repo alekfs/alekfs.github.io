10892b0da9cb4205b41e68f13155c9166de3de32
alfe3528 <alfe3528@colorado.edu>
2023-04-13 14:52:46 -0600

    Errors from WA11

--- wa/wa11/wa11.js
+++ wa/wa11/wa11.js
@@ -1,16 +1,15 @@
 
 
 document.getElementById('githubForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault();
     var username = document.getElementById('githubUsername').value; // Get GitHub username input value for example alekfs
     // Fetch user profile data from GitHub API
     const endpoint = "https://api.github.com/users/"
-    fetch(endpoint + username) // will fetch the Github API for https://api.github.com/users/alekfs
+    fetch(endpoint + username) // will fetch the Github API for if user enters alekfs: https://api.github.com/users/alekfs
     .then(response => response.json())
       .then(data => {
-        // Update HTML with profile data
-        var profileContainer = document.getElementById('profileContainer');
-        profileContainer.innerHTML = `
+        var profile = document.getElementById('profileContainer');
+        profile.innerHTML = `
           <h2>${data.login}</h2>
           <img src="${data.avatar_url}" alt="Avatar">
           <p>Name: ${data.name} </p>
@@ -22,10 +21,8 @@ document.getElementById('githubForm').addEventListener('submit', function(event)
     })
     .catch(error => {
         console.error('Error fetching GitHub profile data:', error);
-        // Display error message in case of API fetch failure
-        var profileContainer = document.getElementById('profileContainer');
-        profileContainer.innerHTML = `<p>Failed to fetch GitHub user data.</p>`;
+        var profile = document.getElementById('profile'); // error message in case of API fetch failure
+        profile.innerHTML = `<p>Failed to fetch GitHub user data.</p>`;
     });
-
 });
 
