f32af7e36f66d8bee2bbb1900d064bcfeedcfa6c
alfe3528 <alfe3528@colorado.edu>
2023-04-18 00:31:26 -0600

    Errors

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -1,15 +1,15 @@
 // Problem 1
 
-let text = '{"employees" :  [' +
-'{ "name":"Sam" , "department":"Tech" , "designation":"Manager" , "salary":"40000" , "raiseEligible":"true"}, ' +
-'{ "name":"Mary" , "department":"Finance" , "designation":"Trainee" , "salary":"18500" , "raiseEligible":"true"}, ' +
-'{ "name":"Bill" , "department":"HR" , "designation":"Executive" , "salary":"21200" , "raiseEligible":"false"} ]}';
+let text = {"employees" :  [ 
+{ "name":"Sam" , "department":"Tech" , "designation":"Manager" , "salary":"40000" , "raiseEligible":"true"},
+{ "name":"Mary" , "department":"Finance" , "designation":"Trainee" , "salary":"18500" , "raiseEligible":"true"},
+{ "name":"Bill" , "department":"HR" , "designation":"Executive" , "salary":"21200" , "raiseEligible":"false"} ]};
 
 const obj = JSON.parse(text);
 console.log("Problem 1");
 console.log(text);
 console.log(obj);
-console.log(obj["employees"][1]["name"]);
+console.log(obj["employees"][0]["name"]);
 
 
 // Problem 2
@@ -27,7 +27,9 @@ let employeeLen = obj["employees"].length;
 obj["employees"][employeeLen + 1] = '{ "name":"Anna" , "department":"Tech" , "designation":"Executive" , "salary":"25600" , "raiseEligible":"false" }';
 
 console.log("Problem 3");
-console.log(text);
+console.log(employeeLen);
+console.log(obj);
+console.log(obj["employees"][2]["name"]);
 
 //Problem 4
 
