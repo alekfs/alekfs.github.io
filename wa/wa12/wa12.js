b760062a07e97f18d2d567cc0e76de5af85a1b0d
alfe3528 <alfe3528@colorado.edu>
2023-04-20 12:10:10 -0600

    WA12 Errors

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -1,9 +1,9 @@
 // Problem 1
 
-let text = {"employees" :  [ 
-{ "name":"Sam" , "department":"Tech" , "designation":"Manager" , "salary":"40000" , "raiseEligible":"true"},
-{ "name":"Mary" , "department":"Finance" , "designation":"Trainee" , "salary":"18500" , "raiseEligible":"true"},
-{ "name":"Bill" , "department":"HR" , "designation":"Executive" , "salary":"21200" , "raiseEligible":"false"} ]};
+let text = '{"employees" :  [' +
+'{ "name":"Sam" , "department":"Tech" , "designation":"Manager" , "salary":"40000" , "raiseEligible":"true"}, ' +
+'{ "name":"Mary" , "department":"Finance" , "designation":"Trainee" , "salary":"18500" , "raiseEligible":"true"}, ' +
+'{ "name":"Bill" , "department":"HR" , "designation":"Executive" , "salary":"21200" , "raiseEligible":"false"} ]}';
 
 const obj = JSON.parse(text);
 console.log("Problem 1");
@@ -14,8 +14,12 @@ console.log(obj["employees"][0]["name"]);
 
 // Problem 2
 
-let company = '{"company" : [' +
-'{"companyName":Tech Stars , "website":"www.techstars.site" , "employees":employees} ]}';
+let company = {
+    "companyName": "Tech Stars", "website": "www.techstars.site", "employees": [
+    { "firstName": "Sam", "department": "Tech", "designation": "Manager", "salary": 40000, "raiseEligible": true},
+    { "firstName": "Mary", "department": "Finance", "designation": "Trainee", "salary": 18500, "raiseEligible": true},
+    { "firstName": "Bill", "department": "HR", "designation": "Executive", "salary": 21200, "raiseEligible": false}]
+}
 
 console.log("Problem 2");
 console.log(company);
@@ -31,5 +35,40 @@ console.log(employeeLen);
 console.log(obj);
 console.log(obj["employees"][2]["name"]);
 
-//Problem 4
+// Problem 4
+function calculateTotalSalary(company) {
+    let totalSalary = 0;
+    const employees = company.employees;
+    for (let i = 0; i < employees.length; i++) {
+      totalSalary += employees[i].salary;
+    }
+    return totalSalary;
+}
 
+console.log("Problem 4");
+console.log(calculateTotalSalary(company));
+
+// Problem 5
+function updateSalary(company) {
+    const employees = company.employees;
+    for (let i = 0; i < employees.length; i++) {
+      if (employees[i].raiseEligible) {
+        employees[i].salary *= 1.1;
+        employees[i].raiseEligible = false;
+      }
+    }
+}
+
+console.log("Problem 5");
+console.log(updateSalary(company));
+
+// Problem 6
+const workingFromHome = ['Anna', 'Sam'];
+
+
+company.employees.forEach(employee => {
+  employee.wfh = workingFromHome.includes(employee.firstName);
+});
+
+console.log("Problem 6");
+console.log(company);
