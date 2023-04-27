67b7fa0d47d99c921bdeea3bb4acf4cb9f6508a7
alfe3528 <alfe3528@colorado.edu>
2023-04-26 21:59:51 -0600

    Errors

--- wa/wa13/wa13.js
+++ wa/wa13/wa13.js
@@ -6,26 +6,27 @@ const headsImageUrl = "heads.png";
 const tailsImageUrl = "tails.png";
 let volume = 0;
 let isHeads = true;
+result.setAttribute('style', 'white-space: pre;');
 
 function flipCoin() {
-coin.style.animation = "none";
-void coin.offsetWidth;
-coin.style.animation = "spin 1s ease-in-out";
+    coin.style.animation = "none";
+    void coin.offsetWidth;
+    coin.style.animation = "spin 1s ease-in-out";
 
-const randomNumber = Math.random();
-    if (randomNumber >= 0.5) {
-        volume += 5;
-        result.textContent = "Heads! Volume increased by 5";
-        coin.style.backgroundImage = `url(${headsImageUrl})`;
-        isHeads = true;
-    } else {
-        volume -= 5;
-        if (volume < 0) volume = 0;
-        result.textContent = "Tails! Volume decreased by 5";
-        coin.style.backgroundImage = `url(${tailsImageUrl})`;
-        isHeads = false;
-    }
-    console.log(`Current volume: ${volume}`);
+    const randomNumber = Math.random();
+        if (randomNumber >= 0.5) {
+            volume += 5;
+            result.textContent = "Heads! \r\n Volume increased by 5";
+            coin.style.backgroundImage = `url(${headsImageUrl})`;
+            isHeads = true;
+        } else {
+            volume -= 5;
+            if (volume < 0) volume = 0;
+            result.textContent = "Tails! \r\n Volume descreased by 5";
+            coin.style.backgroundImage = `url(${tailsImageUrl})`;
+            isHeads = false;
+        }
+        console.log(`Current volume: ${volume}`);
 }
 
 coin.addEventListener("click", flipCoin);
